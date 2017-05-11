417352c794a7f01613a0793e83d899059d6e2e27
Benji <benjamincomley@gmail.com>
2017-05-11 10:42:00 +0200

    rspec tests passing

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table Restaurant' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :address }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :phone }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :address }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryGirl.create(:restaurant)).to be_valid
+    end
+  end
 end
