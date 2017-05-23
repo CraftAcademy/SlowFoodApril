5c5e8607c55511533e3800b6e4490681650e184b
junydania <o.dania@aol.com>
2017-05-23 13:41:23 +0200

    cherry-picked 498235dca05118381a4574799f1d947783af4938 resolved conflicts

--- app/assets/javascripts/mygeo.js
+++ app/assets/javascripts/mygeo.js
@@ -1,33 +1,46 @@
-var latitude;
-var longitude;
+
 function setGeolocation() {
     var test_env = $('#map').data().testEnv;
+    var coords;
+    var latitude;
+    var longitude;
+    debugger;
     if (test_env === false) {
-        GMaps.geolocate({
-            success: function(position) {
-                latitude = position.coords.latitude;
-                longitude = position.coords.longitude;
-                return {latitude: latitude, longitude: longitude};
-            },
-            error: function(error) {
-                alert('Geolocation failed: ' + error.message);
-            },
-            not_supported: function() {
-                alert('Your browser does not support geolocation');
-            }
-
+        // GMaps.geolocate({
+        //     success: function(position) {
+        //         latitude = position.coords.latitude;
+        //         longitude = position.coords.longitude;
+        //         debugger;
+        //         coords =  {latitude: latitude, longitude: longitude};
+        //     },
+        //     error: function(error) {
+        //         alert('Geolocation failed: ' + error.message);
+        //     },
+        //     not_supported: function() {
+        //         alert('Your browser does not support geolocation');
+        //     }
+        //
+        // });
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            coords =  {latitude: lat, longitude: long};
         });
     } else {
         latitude = -25.838572;
         longitude = 28.209190;
-        return {latitude: latitude, longitude: longitude};
+        coords =  {latitude: latitude, longitude: longitude};
     }
+    debugger;
+    return coords;
 }
 
 $(document).ready(function() {
     var lat;
     var long;
     var map;
+
+    setGeolocation();
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -44,53 +57,53 @@ $(document).ready(function() {
     // var longitude = 28.209190;
     // map.setCenter(lat, long);
 
-    GMaps.geolocate({
-        success: function() {
-
-            map.setCenter(lat, long);
-
-            map.addMarker({
-                lat: lat,
-                lng: long,
-                title: 'You',
-                infoWindow: {
-                    content: '<p>Hey You!</p>'
-                }
-            });
-
-            map.drawOverlay({
-                lat: lat,
-                lng: long,
-                layer: 'floatPane',
-                content: '<div class="overlay">Right here!</div>'
-                });
-
-            map.addMarker({
-                lat: lat - 0.01,
-                lng: long - 0.01,
-                title: 'You',
-                infoWindow: {
-                    content: '<p>Hey You!</p>'
-                }
-            });
-
-            map.drawOverlay({
-                lat: lat - 0.01,
-                lng: long - 0.01,
-                layer: 'floatPane',
-                content: '<div class="overlay">Next here!</div>'
-                });
-
-
-        },
-
-        error: function(error) {
-            alert('Geolocation failed: ' + error.message);
-        },
-        not_supported: function() {
-            alert('Your browser does not support geolocation');
-        }
-
-    });
+    // GMaps.geolocate({
+    //     success: function() {
+    //
+    //         map.setCenter(lat, long);
+    //
+    //         map.addMarker({
+    //             lat: lat,
+    //             lng: long,
+    //             title: 'You',
+    //             infoWindow: {
+    //                 content: '<p>Hey You!</p>'
+    //             }
+    //         });
+    //
+    //         map.drawOverlay({
+    //             lat: lat,
+    //             lng: long,
+    //             layer: 'floatPane',
+    //             content: '<div class="overlay">Right here!</div>'
+    //             });
+    //
+    //         map.addMarker({
+    //             lat: lat - 0.01,
+    //             lng: long - 0.01,
+    //             title: 'You',
+    //             infoWindow: {
+    //                 content: '<p>Hey You!</p>'
+    //             }
+    //         });
+    //
+    //         map.drawOverlay({
+    //             lat: lat - 0.01,
+    //             lng: long - 0.01,
+    //             layer: 'floatPane',
+    //             content: '<div class="overlay">Next here!</div>'
+    //             });
+    //
+    //
+    //     },
+    //
+    //     error: function(error) {
+    //         alert('Geolocation failed: ' + error.message);
+    //     },
+    //     not_supported: function() {
+    //         alert('Your browser does not support geolocation');
+    //     }
+    //
+    // });
 
 });
