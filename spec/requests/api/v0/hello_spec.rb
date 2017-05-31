4ba3d0030691db6f0887cc8e56686200019cfe90
junydania <o.dania@aol.com>
2017-05-31 16:19:51 +0200

    removed white space from the spec file

--- spec/requests/api/v0/hello_spec.rb
+++ spec/requests/api/v0/hello_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Api::V0::HelloController, type: :request do
   describe 'GET /v0/hello' do
     it 'should return Hi' do
       get '/api/v0/hello'
-
       expect(response.status).to eq 200
       expect(response_json['message']).to eq 'Hi'
     end
