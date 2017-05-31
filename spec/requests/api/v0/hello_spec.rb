822502a0dfa8c05610259ccbb04a1ec5cbe4afab
mmollerstrom <mmollerstrom@icloud.com>
2017-05-31 07:39:46 -0400

    modifying hello_spec

--- spec/requests/api/v0/hello_spec.rb
+++ spec/requests/api/v0/hello_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Api::V0::HelloController, type: :request do
   describe 'GET /v0/hello' do
     it 'should return Hi' do
       get '/api/v0/hello'
-    end
       json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
       expect(json_response['message']).to eq 'Hi'
     end
   end
+end
