96dc086bb77e2a758b96830bd217066a50b599da
junydania <o.dania@aol.com>
2017-06-01 16:14:35 +0200

    updated the env file

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,7 +16,7 @@ end
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
 Capybara.register_driver :poltergeist do |app|
- Capybara::Poltergeist::Driver.new(app, js_error: false)
+ Capybara::Poltergeist::Driver.new(app, js_errors: false)
 end
 Capybara.javascript_driver = :poltergeist
 
