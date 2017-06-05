ce73277b24f2950a4943fdd94392d44b700020d7
ceca2017 <claesca@outlook.com>
2017-06-05 09:01:59 +0200

    Checkout page with restaurant info (#29)
    * modified cucumber test to add checks for restaurant names and if the cart is empty
    add code to see if cart is empty and then state this on the checkout page
    
    * add restaurant name to the dish on the checkout page
    
    * add restaurant ordering info to the checkout page
    
    * add link to empty cart and removed flash message next to dishes when adding a dish to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,16 +3,32 @@ class CartsController < ApplicationController
     session[:cart] ||= []
     dish = Dish.find(params[:dish_id])
     @restaurant_id = params[:restaurant_id]
-    session[:cart] << dish
+    restaurant = Restaurant.find(@restaurant_id)
+    session[:cart] << [dish, restaurant]
     redirect_to restaurant_menus_path(@restaurant_id)
     flash[:notice] = "#{dish.name} added to cart"
   end
 
   def checkout
-    @items = session[:cart]
-    @total_price = 0
-    @items.each do |item|
-      @total_price += item['price']
+    if session[:cart] == nil
+      @empty_cart = true
+    else
+      @items = session[:cart]
+      @total_price = 0
+      @restaurants = []
+      @items.each do |item|
+        @total_price += item[0]['price']
+        unless @restaurants.include?(item[1])
+          @restaurants << item[1]
+        end
+      end
+      @empty_cart = false
     end
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    @empty_cart = true
+    render 'checkout'
   end
 end
